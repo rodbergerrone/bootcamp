01bcbc47393b3ce809c38aafb0d82136dbde4b3c
rodbergerrone <rodbergerrone@icloud.com>
2019-11-19 21:16:55 +0100

    Homework

--- basic/testy.py
+++ basic/testy.py
@@ -1,11 +1,19 @@
 # x = input("Wpisz coś:")
 # print(count(x))
 
-x = list()
+# x = list()
+# while True:
+#     x.append(input("Wpisz cokolwiek: "))
+#     print(x)
+#     print(x[2: -2])
+#     print(len(x))
+#     if "end" in input("Wpisz 'end' aby zakończyć."):
+#         break
+
+# x = [1, 2, 3, 4]
+# # print(sum(x))
+
+x = []
 while True:
-    x += input("Wpisz cokolwiek: ")
+    x.insert(0, input("Wpisz cokolwiek:"))
     print(x)
-    print(x[2: -2])
-    print(len(x))
-    if "end" in input("Wpisz 'end' aby zakończyć."):
-        break
