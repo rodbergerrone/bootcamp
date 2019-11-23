cad4a86d0bb17dfde5e9f62e36362d89f1e2db81
rodbergerrone <rodbergerrone@icloud.com>
2019-11-23 23:07:05 +0100

    Homework

--- collections/ex-06.py
+++ collections/ex-06.py
@@ -1,21 +1,8 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+lista = [99, 22, 3, 4, 5, 200, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 print("Przed:", lista)
-liczba_min = None
-liczba_max = None
-for liczba in lista:
-    if liczba_min == None or liczba < liczba_min:
-        liczba_min = liczba
-        print("Liczba min:", liczba_min)
-    elif liczba_max == None or liczba > liczba_max:
-        liczba_max = liczba
-        print("Liczba max:", max(lista))
-lista[lista.index(liczba_min)] = liczba_max
-lista[lista.index(liczba_max)] = liczba_min
+print("Najmniejsza liczba:", min(lista))
+print("Największa liczba:", max(lista))
+mini, maxi = min(lista), max(lista)
+index_min, index_max = lista.index(min(lista)), lista.index(max(lista))
+lista[index_min], lista[index_max] = maxi, mini
 print("Po:", lista)
-
-# max(lista)
-# min(lista)
-# lista.index()
-
-# zamienić miejscami najmniejszą i największą
-
