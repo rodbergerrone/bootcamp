27d8fece31868ba5a2f9291bd6bb841644c27b1c
rodbergerrone <rodbergerrone@icloud.com>
2019-11-26 20:20:24 +0100

    Homework

---
+++ basic/cypher Sparta.py
@@ -0,0 +1,25 @@
+a1, a2 = "abcdefghijklmnopqrstuvwxyz1234567890", "cdefghijklmnopqrstuvwxyzab3456789012"
+
+def decrypt(message):
+    cypher = str.maketrans(a2, a1)
+    return message.translate(cypher)
+
+def encrypt(message):
+    cypher = str.maketrans(a1, a2)
+    return message.translate(cypher)
+
+print("""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Witamy w szyfratorze:
+- aby odczytać wiadomość wpisz 'decrypt'
+- aby zakodować wiadomość wpisz 'encrypt'
+- aby zakończyć wpisz 'cokolwiek'
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+o = input("Co chcesz zrobić:")
+if o.lower() == 'decrypt':
+    message = input("Wpisz wiadomość:")
+    print(f"Odszyfrowano:", decrypt(message))
+elif o.lower() == 'encrypt':
+    message = input("Wpisz wiadomość:")
+    print(f"Zaszyfrowano:", encrypt(message))
+else:
+    print("Do zobaczenia")
--- basic/cypher.py
+++
@@ -1,9 +0,0 @@
-a1, a2 = "abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab"
-cypher = str.maketrans(a1, a2)
-
-message = """Kqnglpg urqvmcpkg q ebvgtpcuvgl y utqfg."""
-print (message.translate(cypher))
-
-# do innych messaage.replace(' ', '')
-# jak zrobić aby były w obie strona a1 -> a2 -> a1
-# szyfr Vinengere
